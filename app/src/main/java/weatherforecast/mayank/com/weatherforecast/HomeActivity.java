16f902c3a0432724c3ede4f239c73b2b5187b2f9
Mayank Jain <54439427+mayankjain-sudo@users.noreply.github.com>
2022-03-11 17:45:22 +0530

    code

---
+++ app/src/main/java/weatherforecast/mayank/com/weatherforecast/HomeActivity.java
@@ -0,0 +1,70 @@
+package weatherforecast.mayank.com.weatherforecast;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class HomeActivity extends AppCompatActivity {
+
+    EditText city;
+    Intent intent;
+    Button searchButton;
+    String cityname;
+
+    public static final String CITY_NAME = "CITYNAME";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        searchButton = (Button) findViewById(R.id.searchButton);
+
+        city = (EditText) findViewById(R.id.editTextCityName);
+
+        intent = new Intent(HomeActivity.this, MainActivity.class);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                cityname = city.getText().toString();
+                intent.putExtra(CITY_NAME, cityname);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/weatherforecast/mayank/com/weatherforecast/MainActivity.java
@@ -0,0 +1,172 @@
+package weatherforecast.mayank.com.weatherforecast;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.json.JSONException;
+
+import weatherforecast.mayank.com.weatherforecast.adapter.DailyForecastPageAdapter;
+import weatherforecast.mayank.com.weatherforecast.model.Weather;
+import weatherforecast.mayank.com.weatherforecast.model.WeatherForecast;
+
+public class MainActivity extends FragmentActivity {
+
+
+    private TextView cityText;
+    private TextView condDescr;
+    private TextView temp;
+    private TextView press;
+    private TextView windSpeed;
+    private TextView windDeg;
+    private TextView unitTemp;
+
+    private TextView hum;
+    private ImageView imgView;
+
+    private static String forecastDaysNum = "15";
+    private ViewPager pager;
+    private String cityname;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        cityname = intent.getStringExtra(HomeActivity.CITY_NAME);
+
+        String city = cityname;
+        String lang = "en";
+
+        cityText = (TextView) findViewById(R.id.cityText);
+        temp = (TextView) findViewById(R.id.temp);
+        unitTemp = (TextView) findViewById(R.id.unittemp);
+        unitTemp.setText("°C");
+        condDescr = (TextView) findViewById(R.id.skydesc);
+
+        pager = (ViewPager) findViewById(R.id.pager);
+        imgView = (ImageView) findViewById(R.id.condIcon);
+		/*
+		condDescr = (TextView) findViewById(R.id.condDescr);
+
+		hum = (TextView) findViewById(R.id.hum);
+		press = (TextView) findViewById(R.id.press);
+		windSpeed = (TextView) findViewById(R.id.windSpeed);
+		windDeg = (TextView) findViewById(R.id.windDeg);
+
+		*/
+
+
+        JSONWeatherTask task = new JSONWeatherTask();
+        task.execute(new String[]{city,lang});
+
+        JSONForecastWeatherTask task1 = new JSONForecastWeatherTask();
+        task1.execute(new String[]{city,lang, forecastDaysNum});
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+
+    private class JSONWeatherTask extends AsyncTask<String, Void, Weather> {
+
+        @Override
+        protected Weather doInBackground(String... params) {
+            Weather weather = new Weather();
+            String data = ( (new WeatherHttpClient()).getWeatherData(params[0], params[1]));
+
+            try {
+                weather = JSONWeatherParser.getWeather(data);
+                System.out.println("Weather ["+weather+"]");
+                // Let's retrieve the icon
+                weather.iconData = ( (new WeatherHttpClient()).getImage(weather.currentCondition.getIcon()));
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return weather;
+
+        }
+
+
+        @Override
+        protected void onPostExecute(Weather weather) {
+            super.onPostExecute(weather);
+
+            if (weather.iconData != null && weather.iconData.length > 0) {
+                Bitmap img = BitmapFactory.decodeByteArray(weather.iconData, 0, weather.iconData.length);
+                imgView.setImageBitmap(img);
+            }
+
+
+            cityText.setText(weather.location.getCity() + "," + weather.location.getCountry());
+            temp.setText("" + Math.round((weather.temperature.getTemp() - 275.15)));
+            condDescr.setText(weather.currentCondition.getCondition() + "(" + weather.currentCondition.getDescr() + ")");
+			/*
+
+			temp.setText("" + Math.round((weather.temperature.getTemp() - 275.15)) + "�C");
+			hum.setText("" + weather.currentCondition.getHumidity() + "%");
+			press.setText("" + weather.currentCondition.getPressure() + " hPa");
+			windSpeed.setText("" + weather.wind.getSpeed() + " mps");
+			windDeg.setText("" + weather.wind.getDeg() + "�");
+			*/
+        }
+
+
+
+    }
+
+
+    private class JSONForecastWeatherTask extends AsyncTask<String, Void, WeatherForecast> {
+
+        @Override
+        protected WeatherForecast doInBackground(String... params) {
+
+            String data = ( (new WeatherHttpClient()).getForecastWeatherData(params[0], params[1], params[2]));
+            WeatherForecast forecast = new WeatherForecast();
+            Weather weather = new Weather();
+            try {
+                forecast = JSONWeatherParser.getForecastWeather(data);
+                System.out.println("Weather ["+forecast+"]");
+                // Let's retrieve the icon
+                weather.iconData = ( (new WeatherHttpClient()).getImage(weather.currentCondition.getIcon()));
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return forecast;
+
+        }
+
+
+
+
+        @Override
+        protected void onPostExecute(WeatherForecast forecastWeather) {
+            super.onPostExecute(forecastWeather);
+
+            DailyForecastPageAdapter adapter = new DailyForecastPageAdapter(Integer.parseInt(forecastDaysNum), getSupportFragmentManager(), forecastWeather);
+
+            pager.setAdapter(adapter);
+        }
+
+
+
+    }
+}
